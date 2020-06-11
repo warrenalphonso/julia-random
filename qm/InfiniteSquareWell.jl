399ffd9171a8c95a30d7d083264ad008bfb2eb23
Warren Alphonso <warrenalphonso02@gmail.com>
2020-06-11 10:36:15 -0700

    Animated superposition of first two stationary states

--- qm/InfiniteSquareWell.jl
+++ qm/InfiniteSquareWell.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.9.7
+# v0.9.8
 
 using Markdown
 macro bind(def, element)
@@ -56,7 +56,7 @@ const gen_E(m, a) = (n) -> (n^2 * π^2 * ħ^2)/(2 * m * a^2)
 
 # ╔═╡ 1a51e348-abb7-11ea-056f-ef297ec19c80
 # Function to get probability of finding particle at position x at time t 
-gen_prob(ψ) = (x) -> conj(ψ(x)) * ψ(x)
+gen_prob(ψ) = (x,t) -> real( conj(ψ(x,t)) * ψ(x,t) )
 
 # ╔═╡ e8fbd416-abb6-11ea-2d8b-f3614b2702be
 md"""
@@ -68,29 +68,34 @@ The probability of finding a particle in a stationary state at a certain positio
 # ╔═╡ a849fc4e-abb7-11ea-0847-2190d7ccd188
 begin
 	a = 2
-	m = 1
+	m = 1e-34
+	ω = (π^2 * ħ / (2*m*a^a))
 end
 
 # ╔═╡ f81bf9d2-abb9-11ea-25f7-7ffbbace40c0
 # Vary this slider to change the stationary state
 @bind n Slider(1:15)
 
+# ╔═╡ 55628b2a-abff-11ea-164a-6deb7cd70b1d
+# Varying this slider won't change much for the plot above because stationary states 
+# don't change with time. But it will affect the plot below. 
+@bind t Slider(0:10)
+
 # ╔═╡ ab687ea8-abb9-11ea-2bff-db56b90cd8d5
-begin 
+let 
 	ψ = gen_ψ(a, n)
 	E = gen_E(m, a)
-	prob = gen_prob(ψ)
-end
-
-# ╔═╡ 85eca3ac-abb9-11ea-0d80-f7c53bd7b92f
-begin
-	plot(prob, 0:.01:a)
+	ϕ = gen_ϕ(E(n))
+	prob = gen_prob((x, t) -> ψ(x)*ϕ(t))
+	
+	plot((x) -> prob(x, t), 0:.01:a)
+	title!("Probability density at t=0")
 	xlabel!("x")
 	ylabel!("Probability")
 end
 
 # ╔═╡ 0e0549d2-abbb-11ea-0ee0-cfbd662a355c
-n
+(n, t)
 
 # ╔═╡ 253859d2-abbb-11ea-1f4a-cdd7201b4fba
 md"""
@@ -98,6 +103,49 @@ md"""
 
 We don't get much interesting behavior in stationary states, but if we take a linear combination of stationary states, we suddenly get variation with respect to time in the probability density. 
 """
+
+# ╔═╡ 536959aa-abfe-11ea-252c-bb2e4e68748b
+let 
+	ψ₁ = gen_ψ(a, 1)
+	ψ₂ = gen_ψ(a, 2)
+	E = gen_E(m, a)
+	ϕ₁ = gen_ϕ(E(1))
+	ϕ₂ = gen_ϕ(E(2))
+	normalization_constant = 1/√2 
+	state = (x, t) -> normalization_constant * ( ψ₁(x)*ϕ₁(t) + ψ₂(x)*ϕ₂(t) )
+	prob = gen_prob(state)
+	
+	plot((x) -> prob(x, t), 0:.01:a)
+	
+end
+
+# ╔═╡ 8d9c271a-ac01-11ea-3e94-ad2b10f57efd
+function plot_one_and_two_stationary(t) 
+	ψ₁ = gen_ψ(a, 1)
+	ψ₂ = gen_ψ(a, 2)
+	E = gen_E(m, a)
+	ϕ₁ = gen_ϕ(E(1))
+	ϕ₂ = gen_ϕ(E(2))
+	normalization_constant = 1/√2 
+	state = (x, t) -> normalization_constant * ( ψ₁(x)*ϕ₁(t) + ψ₂(x)*ϕ₂(t) )
+	prob = gen_prob(state)
+	
+	x = 0:0.01:a
+	y = imag(state.(x, t))
+	z = real(state.(x, t))
+	plot(x,y,z, xaxis="x", yaxis="Imaginary component", zaxis="Real component", 
+		 xlims=(0, a), ylims=(-1,1), zlims=(-1,1), label="Wavefunction ψ", 
+		 fillrange = 0)
+	plot!(x, [0 for i in x], prob.(x, t), label="PDF", fillrange=0)
+end
+
+# ╔═╡ 9038472e-ac06-11ea-047f-7f38d158b192
+let 
+	anim = @animate for t in 0 : ω/50 : 3ω
+		plot_one_and_two_stationary(t)
+	end
+	gif(anim, "one_and_two_stationary.gif", fps=15)
+end
 
 # ╔═╡ Cell order:
 # ╟─157e319a-abac-11ea-1feb-abd64534887b
@@ -112,7 +160,10 @@ We don't get much interesting behavior in stationary states, but if we take a li
 # ╟─e8fbd416-abb6-11ea-2d8b-f3614b2702be
 # ╠═a849fc4e-abb7-11ea-0847-2190d7ccd188
 # ╠═ab687ea8-abb9-11ea-2bff-db56b90cd8d5
-# ╠═85eca3ac-abb9-11ea-0d80-f7c53bd7b92f
 # ╠═f81bf9d2-abb9-11ea-25f7-7ffbbace40c0
+# ╠═55628b2a-abff-11ea-164a-6deb7cd70b1d
 # ╠═0e0549d2-abbb-11ea-0ee0-cfbd662a355c
 # ╟─253859d2-abbb-11ea-1f4a-cdd7201b4fba
+# ╠═536959aa-abfe-11ea-252c-bb2e4e68748b
+# ╠═8d9c271a-ac01-11ea-3e94-ad2b10f57efd
+# ╠═9038472e-ac06-11ea-047f-7f38d158b192
